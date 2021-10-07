60f48820105bdd14adeea4d356415c27d596225b
Chaffinch21 <lena_431@mail.ru>
2021-10-07 11:25:09 +0700

    header and hero questions

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 const sliderEl = document.querySelector('.swiper-container-js');
 let mySwiper = new Swiper (sliderEl, {
   loop: true,
+  zoom: {
+    maxRatio: 5,
+  },
   autoplay: {
     delay: 4000,
   },
@@ -13,3 +16,18 @@ let mySwiper = new Swiper (sliderEl, {
       crossFade: true
     }
 });
+
+const selectEl = document.querySelector('.selectCustom');
+const choices = new Choices(selectEl, {
+  searchEnabled: false,
+  shouldSort: false,
+  duplicateItemsAllowed: false,
+    searchChoices: false,
+  // itemSelectText : ''
+})
+const choicesDropdawn = document.querySelector('.choices__list--dropdown');
+const simpleBar = new SimpleBar(choicesDropdawn);
+
+choicesDropdawn.addEventListener('click', function(){
+    simpleBar.recalculate();
+})
