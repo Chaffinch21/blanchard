98cc42d3079f9b1548b96d9c49dd5a7247fa785d
Chaffinch21 <lena_431@mail.ru>
2021-11-03 21:31:00 +0700

    catalog add

--- script/script.js
+++ script/script.js
@@ -6,6 +6,11 @@ const galleryModalBlock = document.querySelector('.modal-gallery__js');
 const catalogList = document.querySelector('.swiper-wrapper');
 const catalogBtns = document.querySelector('#selectCustom');
 const galleryPagination = document.querySelector('.swiper-pagination');
+const catalogDescr = document.querySelector('.catalog-descr');
+const cauntryBtns = document.querySelectorAll('.country-btn');
+const hronoList = document.querySelectorAll('.accordion-painterlist');
+const hronoItem = document.querySelectorAll('.accordion-hronoitem');
+const accordionBtns = document.querySelectorAll('.accordion-hronoitem__btn');
 
 
 //открытие и закрытие подменю
@@ -23,7 +28,6 @@ submenuBtn.forEach(toggle => {
   })
 })
 
-
 // кастомный селект
 const selectEl = document.querySelector('#selectCustom');
 const choices = new Choices(selectEl, {
@@ -33,12 +37,6 @@ const choices = new Choices(selectEl, {
   renderChoiceLimit: 3,
   renderSelectedChoices: false,
 })
-
-// const containerOuter = document.querySelector('.choise')
-// containerOuter.addEventListener('showDropdown', function(){
-//   console.log(1)
-// })
-
 
 // слайдер галерея
 let swiperGallery = new Swiper(".mySwiper", {
@@ -59,6 +57,8 @@ let swiperGallery = new Swiper(".mySwiper", {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// Галерея загрузка
 
 const loadGallery = (art) => {
   catalogList.innerHTML = '';
@@ -82,7 +82,6 @@ const loadGallery = (art) => {
         }
         swiperGallery.update();
         const galleryBtn = document.querySelectorAll('.gallery-btn__js');
-        console.log(galleryBtn);
 
         const loadModal = (id) =>{
     
@@ -135,3 +134,96 @@ catalogBtns.addEventListener('change', (e) => {
     const art = e.currentTarget.value;
     loadGallery(art);
   });
+
+  // Каталог
+
+  $( function() {
+    $( ".accordion" ).accordion({
+      heightStyle: "content",
+      refresh: true,
+      active: false,
+      animate: 300,
+      collapsible: true,
+    });
+  })
+
+  // setTimeout('$(".accordion").accordion({heightStyle: "content", refresh: true, collapsible: true, active: 1,})', 1000);
+
+  const loadCatalog = (country) => {
+    catalogDescr.innerHTML = '';
+    fetch('data/country.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        for (item of data) {
+          
+          if (item.country == country) {
+            catalogDescr.innerHTML = `${item.text}`;                    
+          }
+
+        }
+      });
+  };
+
+  loadCatalog('italy');
+  console.log(accordionBtns);
+    fetch('data/painter.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((painters) => {
+        accordionBtns.forEach(el => {
+            el.addEventListener('click', function(ev){
+              ev.currentTarget.nextElementSibling.innerHTML = '';
+              for (man of painters) {
+                const countryActive = document.querySelector('.country-btn--active').dataset.country;
+                const year = ev.currentTarget.dataset.year;
+                if ((man.country == countryActive) &&(man.year == year)){
+                  ev.currentTarget.nextElementSibling.innerHTML += `
+                  <li class="accordion-painteritem">
+                    <button class="btn accordion-painteritem__btn" type="button" data-painter = "${man.painter}">${man.painter}</button>
+                  </li>
+                  `;
+                }
+                const painterItemBtns = document.querySelectorAll('.accordion-painteritem__btn');
+                const catalogPainter = document.querySelector('.catalog-painter');
+                painterItemBtns.forEach(el=>{
+                  
+                  el.addEventListener('click', function(){
+                    catalogPainter.innerHTML = '';
+                    console.log(man.painter);
+                    for (man of painters){
+                      if (man.painter == el.textContent){
+                        catalogPainter.innerHTML = `
+                          <img class="painter-img" src="${man.img}" alt="${man.painter}">
+                          <h3 class="painter-name">${man.painter}</h3>
+                          <span class="painter-year">${man.years}</span>
+                          <p class="painter-descr">${man.descr}</p>
+                        `;
+                      }
+                    }
+                    
+                  })
+                })
+              }   
+                
+              })
+          })
+     
+      }); 
+  
+  cauntryBtns.forEach(el => {
+    el.addEventListener('click', (e) => {
+      const country = e.currentTarget.dataset.country;
+      cauntryBtns.forEach(el => { el.classList.remove('country-btn--active'); });
+
+      e.currentTarget.classList.add('country-btn--active');
+      loadCatalog(country);
+      $( ".accordion" ).accordion("refresh");
+       $( ".accordion" ).accordion( "option", "activate", false );
+      hronoList.forEach(el =>{
+        el.innerHTML='';
+      })
+    });
+  })
